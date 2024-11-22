ae2184909d53029297b41f35d7dd5b2ab78c46b5
SerHemb <iserhiy888@gmail.com>
2024-11-22 09:29:36 -0600

    web xml mapping added

--- src/main/java/com/hembei/servlet/ParametrsPrint.java
+++ src/main/java/com/hembei/servlet/ParametrsPrint.java
@@ -10,14 +10,13 @@ import java.io.IOException;
 public class ParametrsPrint extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("WEB-INF/views/index.jsp").forward(request, response);
-//        String kmString = request.getParameter("km");
-//        String kgString = request.getParameter("kg");
-//
-//        int km = Integer.parseInt(kmString);
-//        double kg = Double.parseDouble(kgString);
-//
-//        System.out.println("KM = " + km);
-//        System.out.println("KG = " + kg);
+        String kmString = request.getParameter("km");
+        String kgString = request.getParameter("kg");
+
+        int km = Integer.parseInt(kmString);
+        double kg = Double.parseDouble(kgString);
+
+        System.out.println("KM = " + km);
+        System.out.println("KG = " + kg);
     }
 }
